db6828c7f890e37cff19e17b66f30caca1f42864
Uttam Khanal <kingofdelphi1992@gmail.com>
2019-12-02 16:55:31 +0545

    Add routes redirection for frontend and /api routes

--- django/backend/backend/urls.py
+++ django/backend/backend/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', index, name='index'),
 ]
