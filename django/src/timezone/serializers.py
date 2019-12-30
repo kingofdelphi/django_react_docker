28be8e5e657a4a40dbac105f7d6b5be4127885e4
Uttam Khanal <kingofdelphi1992@gmail.com>
2019-12-30 12:13:41 +0545

    Improve backend validation for time difference

--- django/src/timezone/serializers.py
+++ django/src/timezone/serializers.py
@@ -9,12 +9,23 @@ class TimeZoneSerializer(serializers.ModelSerializer):
     # this doesnot get serialized in the json output
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
-    regex = re.compile(r"(\+|-)\s\d\d?:\d\d?")
+    regex = re.compile(r"(\+|-)\s(\d\d?):(\d\d?)")
     
     def validate_difference_to_GMT(self, value):
-        if not self.regex.match(value):
+        match_info = self.regex.match(value)
+        if not match_info:
             raise serializers.ValidationError("Format must be +/- H:M e.g. + 5:45")
-        return value
+        sign = 1 if match_info.group(1) == '+' else -1
+        hours = int(match_info.group(2))
+        mins = int(match_info.group(3))
+        minDiff = -12 * 60
+        maxDiff = 14 * 60
+
+        minutes = sign * (hours * 60 + mins)
+
+        if sign * hours >= -12 and sign * hours <= 14 and mins < 60 and minDiff <= minutes <= maxDiff:
+            return value
+        raise serializers.ValidationError("Value must be within range [-12:00, +14:00] ")
 
     class Meta:
         model = TimeZone
