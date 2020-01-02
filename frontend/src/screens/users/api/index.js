44e0bd86b71de949db3fd461a1ef17cf2c631f0d
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-01-02 13:54:34 +0545

    Add trailing slash

--- frontend/src/screens/users/api/index.js
+++ frontend/src/screens/users/api/index.js
@@ -8,7 +8,7 @@ export const get_users = (success_callback, failure_callback) => ({
 
 
 export const delete_user = (id, success_callback, failure_callback) => ({
-  url: `/users/${id}`,
+  url: `/users/${id}/`,
   method: 'DELETE',
   with_auth: true,
   success_callback,
