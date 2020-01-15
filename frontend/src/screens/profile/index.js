b551380135b2d4b77051852bde178f15c806b25c
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-01-15 17:38:57 +0545

    Added hooks on profile

--- frontend/src/screens/profile/index.js
+++ frontend/src/screens/profile/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Button from '../../components/button';
@@ -11,108 +12,79 @@ import { changePassword } from './api';
 
 import styles from './styles.module.scss';
 
-class Profile extends React.PureComponent {
-  state = {
-    showSuccess: false,
-    current_password: '',
-    password: '',
-    password1: '',
-    fieldErrors: {},
-  };
+function Profile(props) {
 
-  onCurrentPasswordChange = (event) => {
-    this.setState({ 
-      current_password: event.target.value,
-    });
-  };
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [current_password, setCurrentPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [fieldErrors, setFieldErrors] = useState({});
 
-  onPasswordChange = (event) => {
-    this.setState({ 
-      password: event.target.value,
-    });
-  };
-
-  onPassword1Change = (event) => {
-    this.setState({ 
-      password1: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const data = {
-      current_password: this.state.current_password,
-      password: this.state.password,
-      password1: this.state.password1,
-    };
-    this.props.makeApiCall(
-      changePassword(this.props.loginInfo.id, data),
-      (msg) => {
-        this.setState({ showSuccess: true, fieldErrors: {} });
-      },
-      (message, errorObj) => {
-        this.setState({ 
-          fieldErrors: errorObj
-        });
-      },
-    );
-  };
-
-  render() {
-    const {
       current_password,
       password,
       password1,
-      fieldErrors,
-      showSuccess,
-    } = this.state;
-    return (
-      <div className={styles.main}>
-          { showSuccess && (
-            <MessageBox message="Your password has been changed successully." onSubmit={() => this.setState({ showSuccess: false })} /> 
-          )
-          }
-        <div>
-          <h3>Change Password</h3>
-            <form className={styles.form} onSubmit={this.handleSubmit}>
-              <Input 
-                active
-                autoComplete="current-password"
-                type="password"
-                value={current_password} 
-                label="Current Password" 
-                invalid={fieldErrors['current_password']}
-                validationMessage={fieldErrors['current_password']}
-                onChange={this.onCurrentPasswordChange}
-              />
-              <Input 
-                autoComplete="current-password"
-                type="password"
-                value={password} 
-                label="Confirm Password" 
-                invalid={fieldErrors['passwords'] || fieldErrors['password']}
-                validationMessage={fieldErrors['password']}
-                onChange={this.onPasswordChange}
-              />
-              <Input 
-                type="password"
-                value={password1} 
-                label="Password" 
-                invalid={fieldErrors['passwords'] || fieldErrors['password1']}
-                validationMessage={fieldErrors['password1']}
-                onChange={this.onPassword1Change}
-              />
-              {
-                (fieldErrors['passwords'] || []).map((message, i) => (
-                  <span key={i} className={styles.password_validation}>{message}</span>
-                ))
-              }
-            <Button>Submit</Button>
-          </form>
-        </div>
+    };
+    props.makeApiCall(
+      changePassword(props.loginInfo.id, data),
+      (msg) => {
+        setShowSuccess(true);
+        setFieldErrors({});
+      },
+      (message, errorObj) => {
+        setFieldErrors(errorObj);
+      },
+    );
+  };
+  return (
+    <div className={styles.main}>
+        { showSuccess && (
+          <MessageBox message="Your password has been changed successully." onSubmit={() => setShowSuccess(false)} /> 
+        )
+        }
+      <div>
+        <h3>Change Password</h3>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <Input 
+              active
+              autoComplete="current-password"
+              type="password"
+              value={current_password} 
+              label="Current Password" 
+              invalid={fieldErrors['current_password']}
+              validationMessage={fieldErrors['current_password']}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
+            <Input 
+              autoComplete="password"
+              type="password"
+              value={password} 
+              label="Confirm Password" 
+              invalid={fieldErrors['passwords'] || fieldErrors['password']}
+              validationMessage={fieldErrors['password']}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input 
+              autoComplete="confirm-password"
+              type="password"
+              value={password1} 
+              label="Password" 
+              invalid={fieldErrors['passwords'] || fieldErrors['password1']}
+              validationMessage={fieldErrors['password1']}
+              onChange={(e) => setPassword1(e.target.value)}
+            />
+            {
+              (fieldErrors['passwords'] || []).map((message, i) => (
+                <span key={i} className={styles.password_validation}>{message}</span>
+              ))
+            }
+          <Button>Submit</Button>
+        </form>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({ 
