497b5f1f04a81d9100652c1e7d9de59369db28a9
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-01-15 17:43:43 +0545

    Use hooks in register page

--- frontend/src/screens/register/index.js
+++ frontend/src/screens/register/index.js
@@ -1,45 +1,37 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
+import { useState } from 'react';
 
 import UserDetail from '../components/userdetail';
 
 import withAPIHelper from '../../middleware/api/util';
 import styles from './styles.module.scss';
 
-class Register extends React.PureComponent {
-  state = {
-    registered: {}
+function Register(props) {
+  const [registered, setRegistered] = useState(null);
+
+  const handleSubmit = (userinfo) => {
+    setRegistered(userinfo);
   }
 
-  handleSubmit = (userinfo) => {
-    this.setState({ registered: userinfo });
-  }
-
-  render() {
-    const {
-      registered,
-    } = this.state;
-
-    if (registered.username) {
-      return (
-        <div className={styles.registered}>
-          <h3>Thank you for registering, <span>{registered.username}</span></h3>
-          You can now use the login page and enter your credentials inorder to use this app.
-        </div>
-      );
-    }
-
+  if (registered) {
     return (
-      <div className={styles.main}>
-        <header className={styles.header}>
-          <div>
-            Enter proper username and password and click the Register button.
-          </div>
-        </header>
-        <UserDetail className={styles.form} onSubmit={this.handleSubmit} />
+      <div className={styles.registered}>
+        <h3>Thank you for registering, <span>{registered.username}</span></h3>
+        You can now use the login page and enter your credentials inorder to use this app.
       </div>
     );
   }
+
+  return (
+    <div className={styles.main}>
+      <header className={styles.header}>
+        <div>
+          Enter proper username and password and click the Register button.
+        </div>
+      </header>
+      <UserDetail className={styles.form} onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
-export default withRouter(withAPIHelper(Register));
+export default withAPIHelper(Register);
